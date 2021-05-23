29ff9411c80f8848275f822a22360c2126b33878
MrParkerZ7 <neverzero9@gmail.com>
2021-05-23 18:09:20 +0700

    [Add] prototype spring-cloud-function

---
+++ prototype/spring-cloud-function/src/test/java/puck/cloud/function/springcloudfunction/SpringCloudFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package puck.cloud.function.springcloudfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
