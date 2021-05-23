7495d87c6402cf5d96cff0a6a295422c42bc248f
MrParkerZ7 <neverzero9@gmail.com>
2021-05-23 18:45:58 +0700

    [Update] Unit test

--- prototype/spring-cloud-function/src/main/java/puck/cloud/function/springcloudfunction/SpringCloudFunctionApplication.java
+++ prototype/spring-cloud-function/src/main/java/puck/cloud/function/springcloudfunction/SpringCloudFunctionApplication.java
@@ -2,12 +2,19 @@ package puck.cloud.function.springcloudfunction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
 
 @SpringBootApplication
 public class SpringCloudFunctionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudFunctionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudFunctionApplication.class, args);
+    }
 
+    @Bean
+    public Function<String, Boolean> containsCloud() {
+        return value -> value.contains("cloud");
+    }
 }
--- prototype/spring-cloud-function/src/test/java/puck/cloud/function/springcloudfunction/SpringCloudFunctionApplicationTests.java
+++ prototype/spring-cloud-function/src/test/java/puck/cloud/function/springcloudfunction/SpringCloudFunctionApplicationTests.java
@@ -1,13 +1,37 @@
 package puck.cloud.function.springcloudfunction;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringCloudFunctionApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void doesContainsCloud() throws Exception {
+		MvcResult result = this.mockMvc.perform(post("/containsCloud").contentType(MediaType.TEXT_PLAIN).content("this is a cloud")).andReturn();
+		mockMvc.perform(asyncDispatch(result)).andExpect(content().string("true"));
+	}
+
+	@Test
+	public void doesNotContainsCloud() throws Exception {
+		MvcResult result = this.mockMvc.perform(post("/containsCloud").contentType(MediaType.TEXT_PLAIN).content("this is a function")).andReturn();
+		mockMvc.perform(asyncDispatch(result)).andExpect(content().string("false"));
+	}
 }
